61ea6280a2b3c86edbf3bcad6cf40ecf4ead94b7
Charles Pawling <cpawling@umd.edu>
2021-03-03 23:17:34 -0500

    added bulma box and change .search to .input

--- public/script.js
+++ public/script.js
@@ -37,7 +37,7 @@ function displayMatches() {
   suggestions.innerHTML = html;
 }
 
-const searchInput = document.querySelector('.search');
+const searchInput = document.querySelector('.input');
 const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
